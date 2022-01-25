7f9e2341147bb605f03566d733dfbe2818e344de
Otávio Campos de Abreu Serra <otavio@b2make.com>
2022-01-25 11:42:27 -0300

    TEste de mudança 2

--- modulos/testes/testes.php
+++ modulos/testes/testes.php
@@ -4,7 +4,7 @@ global $_GESTOR;
 
 $_GESTOR['modulo-id']							=	'testes';
 $_GESTOR['modulo#'.$_GESTOR['modulo-id']]		=	Array(
-	'versao' => '1.0.0',
+	'versao' => '1.0.1',
 	'bibliotecas' => Array('interface','html','pagina'),
 	'tabela' => Array(
 		'nome' => 'template',
