a1ce797ad5fc859735cd0463af9da29c1f452c15
Otávio Campos de Abreu Serra <otavio@b2make.com>
2022-02-02 17:26:38 -0300

    Mudança do listener do ícone sanduíche do menu principal: 9

--- assets/global/global.js
+++ assets/global/global.js
@@ -8,12 +8,19 @@ $(document).ready(function(){
 	;
 	
 	if($('._gestor-menuPrincipalMobile').length > 0){
+		var sidebarShow = false;
+		
 		$('._gestor-menuPrincipalMobile').css('cursor','pointer');
 		$('._gestor-menuPrincipalMobile').on('mouseup tap',function(e){
 			if(e.which != 1 && e.which != 0 && e.which != undefined) return false;
 			
 			$('#entrey-menu-principal').show();
-			$('#entrey-menu-principal').sidebar('toggle');
+			
+			if(sidebarShow){
+				$('#entrey-menu-principal').sidebar('hide');
+			} else {
+				$('#entrey-menu-principal').sidebar('show');
+			}
 		});
 	}
 	
