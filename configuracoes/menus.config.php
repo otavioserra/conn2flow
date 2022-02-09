7c907b7bd4a4a74c9f2bd33f7e0c631c186f7e50
Otávio Campos de Abreu Serra <otavio@b2make.com>
2022-02-09 18:12:43 -0300

    Inclusão do arquivo de configuração do menus.

---
+++ configuracoes/menus.config.php
@@ -0,0 +1,26 @@
+<?php
+
+/**********
+	Descrição: Menus configurações.
+**********/
+
+// ===== Definições da variável.
+
+$configuracao = Array(
+	'menu-principal' => Array(
+		Array(
+			'id' => 'sobre',
+			'label' => 'menu-principal-data',
+		),
+		Array(
+			'id' => 'data',
+			'label' => 'menu-principal-data',
+		),
+	),
+);
+
+// ===== Retorno da variável.
+
+return $configuracao;
+
+?>
