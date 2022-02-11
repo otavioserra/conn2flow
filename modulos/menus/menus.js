703b2a7af55aafdee818f92de82e5eb2997b4a4a
Otávio Campos de Abreu Serra <otavio@b2make.com>
2022-02-11 09:42:04 -0300

    [pagina-inicial] inclusão da imagem padrão de banners

--- modulos/menus/menus.js
+++ modulos/menus/menus.js
@@ -41,6 +41,67 @@ $(document).ready(function(){
 			$('.ui.accordion')
 				.accordion()
 			;
+			
+			var itens = {};
+			var currentMousePos = { x: -1, y: -1, mX: 20, mY: 30 };
+			var currentElementClone;
+			
+			$('.menu-item').on('dragstart',function(e){
+				var objPai = $(this).parents('.menu-itens-cont');
+				
+				// ===== Pegar a posição de todos os itens.
+				
+				var posicoes = new Array();
+				
+				objPai.find('.menu-item').each(function(){
+					posicoes.push({
+						
+					});
+				});
+				
+				itens.posicoes = posicoes;
+				
+				// ===== Criar ou modificar área de drop.
+				
+				
+				
+				// ===== Elemento clone.
+				
+				currentElementClone = $(this).clone();
+				
+				currentElementClone.css('position','absolute');
+				
+				currentElementClone.css('top',$(this).position().top);
+				currentElementClone.css('left',$(this).position().left);
+				currentElementClone.css('width',$(this).outerWidth(true));
+				currentElementClone.css('height',$(this).outerHeight(true));
+				currentElementClone.css('backgroundColor','#FFF');
+				
+				currentElementClone.hide();
+				
+				currentElementClone.appendTo($('#menu-pagina-inicial'));
+				
+				// ===== Esconder elemento.
+				
+				$(this).hide();
+			});
+			
+			$('.menu-item').on('drag',function(e){
+				currentElementClone.show();
+				currentElementClone.css('top',currentMousePos.y);
+				currentElementClone.css('left',currentMousePos.x);
+			});
+			
+			$('.menu-item').on('dragend',function(e){
+				currentElementClone.remove();
+				
+				$(this).show();
+			});
+			
+			$('.menu-itens-cont').on('dragover',function(e){
+				currentMousePos.x = e.pageX - $(this).offset().left - currentMousePos.mX;
+				currentMousePos.y = e.pageY - $(this).offset().top - currentMousePos.mY;
+			});
 		}
 		
 		menus_iniciar();
