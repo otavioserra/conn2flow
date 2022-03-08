f30f249fe66826763f134f64f70579ce23f1ac25
Otávio Campos de Abreu Serra <otavio@b2make.com>
2022-03-08 15:20:39 -0300

    [plataforma-app] correção erros: #4

--- controladores/plataforma-app/plataforma-app.php
+++ controladores/plataforma-app/plataforma-app.php
@@ -27,9 +27,11 @@ function plataforma_app_login(){
 	
 	// ===== Validador provisório!!!
 	
-	plataforma_app_200();
 	
 	if(sha1($_REQUEST['appID']) !== 'a45aa0844e67182bf608916891e6080a7d436dc8'){
+		plataforma_app_200(Array(
+			'comparacao' => sha1($_REQUEST['appID']).' !== a45aa0844e67182bf608916891e6080a7d436dc8'
+		));
 		plataforma_app_401();
 	}
 	
