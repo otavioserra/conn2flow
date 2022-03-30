c889a2ad16e5496a684ddbc600c592329e0928cf
Otávio Campos de Abreu Serra <otavio@b2make.com>
2022-03-30 10:51:58 -0300

    [interface] correção do erros: #6

--- gestor.php
+++ gestor.php
@@ -2517,8 +2517,6 @@ function gestor_roteador(){
 	
 	// ===== Disparar o módulo caso houver e devolver a página ou dados ajax ou alterar opções e redirecionar para a raiz do módulo.
 	
-	echo 'AJAX>> '.print_r($_REQUEST,true)."<br>";exit;
-	
 	if($_GESTOR['ajax']){
 		if(isset($paginas)){
 			$modulo = $_GESTOR['modulo'];
