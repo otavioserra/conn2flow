e051c9e41a999395c749dfa8015ff1ff8cf83347
Otávio Campos de Abreu Serra <otavio@b2make.com>
2022-03-30 11:08:48 -0300

    [interface] correção do erros: #14

--- modulos/host-configuracao/host-configuracao.php
+++ modulos/host-configuracao/host-configuracao.php
@@ -2774,9 +2774,6 @@ function host_configuracao_start(){
 	
 	gestor_incluir_bibliotecas();
 	
-	echo 'ajax>> '.$_GESTOR['ajax']."\n";
-	echo '_REQUEST>> '.print_r($_REQUEST,true)."<br>";exit;
-	
 	if($_GESTOR['ajax']){
 		interface_ajax_iniciar();
 		
@@ -2784,7 +2781,11 @@ function host_configuracao_start(){
 			case 'plataforma-testes': host_configuracao_ajax_plataforma_testes(); break;
 		}
 		
+		echo 'finalizar>> '.print_r($_REQUEST,true)."\n";
+		
 		interface_ajax_finalizar();
+		
+		echo '_REQUEST>> '.print_r($_REQUEST,true)."<br>";exit;
 	} else {
 		host_configuracao_interfaces_padroes();
 		
