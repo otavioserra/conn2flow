7e99cdd91c9e5e19d734c00fdfa9953d2cc66c59
Otávio Campos de Abreu Serra <otavio@b2make.com>
2022-05-06 14:14:01 -0300

    [comunicao-configuracoes] criação do arquivo comunicacao-configuracoes.config.php

---
+++ configuracoes/comunicacao-configuracoes.config.php
@@ -0,0 +1,17 @@
+<?php
+
+/**********
+	Descrição: Arquivo de configurações das comunicações.
+**********/
+
+// ===== Definições da variável.
+
+$configuracao = Array(
+	//'id' => 'valor',
+);
+
+// ===== Retorno da variável.
+
+return $configuracao;
+
+?>
