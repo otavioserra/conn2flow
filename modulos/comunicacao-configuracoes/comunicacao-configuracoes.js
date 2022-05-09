dd59f7890fae81590a752ce35e211d8940e33a67
Otávio Campos de Abreu Serra <otavio@b2make.com>
2022-05-09 15:09:28 -0300

    [comunicacao-configuracoes] disparador loading: #4

--- modulos/comunicacao-configuracoes/comunicacao-configuracoes.js
+++ modulos/comunicacao-configuracoes/comunicacao-configuracoes.js
@@ -1,12 +1,16 @@
 $(document).ready(function(){
 	
 	if($('.enviarEmailTeste').length > 0){
+		console.log('enviarEmailTeste loaded');
+		
 		$('enviarEmailTeste').on('mouseup tap',function(e){
 			if(e.which != 1 && e.which != 0 && e.which != undefined) return false;
 			
 			gestor.interface.emailTesteURL = './?_gestor-atualizar=sim';
 			
 			$('.ui.modal.confirm').modal('show');
+			
+			console.log('enviarEmailTeste click');
 		});
 		
 		$('.ui.modal.confirm').modal({
