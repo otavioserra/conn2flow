2a779513ac305c7832564d60a37ef5d54d0f9804
Otávio Campos de Abreu Serra <otavio@b2make.com>
2022-05-12 16:28:11 -0300

    [agendamentos] módulo setup inicial

--- plugins/agendamentos/remoto/modulos/agendamentos/agendamentos.php
+++ plugins/agendamentos/remoto/modulos/agendamentos/agendamentos.php
@@ -1,1 +1,88 @@
-modulo agendamentos
+<?php
+
+global $_GESTOR;
+
+$_GESTOR['modulo-id']							=	'agendamentos';
+$_GESTOR['modulo#'.$_GESTOR['modulo-id']]		=	Array(
+	'versao' => '1.0.0',
+);
+
+// ===== Funções Auxiliares
+
+// ===== Funções Principais
+
+function agendamentos_padrao(){
+	global $_GESTOR;
+	
+	// ===== Iniciar as bibliotecas necessárias.
+	
+	gestor_incluir_biblioteca('pagina');
+	gestor_incluir_biblioteca('formato');
+	gestor_incluir_biblioteca('interface');
+	
+	// ===== Alterações no layout da página.
+	
+	gestor_incluir_biblioteca('layout');
+	
+	layout_trocar_variavel_valor('layout#step','');
+	layout_trocar_variavel_valor('layout#step-mobile','');
+	
+	// ===== Finalizar o layout com as variáveis padrões.
+	
+	layout_loja();
+	
+	// ===== Finalizar interface.
+	
+	interface_componentes_incluir(Array(
+		'componente' => Array(
+			'modal-carregamento',
+			'modal-alerta',
+		)
+	));
+	
+	interface_finalizar();
+	
+	// ===== Incluir o JS.
+	
+	gestor_pagina_javascript_incluir('modulos');
+}
+
+// ==== Ajax
+
+function agendamentos_ajax_padrao(){
+	global $_GESTOR;
+	
+	// ===== Retorno do AJAX.
+	
+	$_GESTOR['ajax-json'] = Array(
+		'status' => 'OK',
+	);
+}
+
+// ==== Start
+
+function agendamentos_start(){
+	global $_GESTOR;
+	
+	// ===== Verificar se o usuário está logado.
+	
+	gestor_permissao();
+	
+	// ===== Opções da interface, senão executar padrão.
+	
+	if($_GESTOR['ajax']){
+		switch($_GESTOR['ajax-opcao']){
+			//case 'opcao': agendamentos_ajax_opcao(); break;
+			default: agendamentos_ajax_padrao();
+		}
+	} else {
+		switch($_GESTOR['opcao']){
+			//case 'opcao': agendamentos_opcao(); break;
+			default: agendamentos_padrao();
+		}
+	}
+}
+
+agendamentos_start();
+
+?>
