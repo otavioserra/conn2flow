aab77725ae7e1a93b55066db9ff1a97cb82e2e8f
Otávio Campos de Abreu Serra <otavio@b2make.com>
2022-05-17 10:24:50 -0300

    [agendamentos] teste dos botões de mudança de tela.

--- plugins/agendamentos/remoto/modulos/agendamentos/agendamentos.js
+++ plugins/agendamentos/remoto/modulos/agendamentos/agendamentos.js
@@ -1,7 +1,19 @@
 $(document).ready(function(){
 	
 	function start(){
+		$('.agendarBtn').on('mouseup tap',function(e){
+			if(e.which != 1 && e.which != 0 && e.which != undefined) return false;
+			
+			$('.agendamentosTela').hide();
+			$('.agendar').show();
+		});
 		
+		$('.agendamentosBtn').on('mouseup tap',function(e){
+			if(e.which != 1 && e.which != 0 && e.which != undefined) return false;
+			
+			$('.agendamentosTela').hide();
+			$('.agendamentos').show();
+		});
 	}
 	
 	start();
--- plugins/agendamentos/remoto/modulos/agendamentos/agendamentos.php
+++ plugins/agendamentos/remoto/modulos/agendamentos/agendamentos.php
@@ -4,7 +4,7 @@ global $_GESTOR;
 
 $_GESTOR['modulo-id']							=	'agendamentos';
 $_GESTOR['modulo#'.$_GESTOR['modulo-id']]		=	Array(
-	'versao' => '1.0.0',
+	'versao' => '1.0.1',
 );
 
 // ===== Funções Auxiliares
