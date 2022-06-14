247101fb27a409405e622cb0078da2b56c3a62be
Otávio Campos de Abreu Serra <otavio@b2make.com>
2022-06-14 10:05:50 -0300

    [agendamentos] modulo inicial

---
+++ plugins/agendamentos/local/modulos/agendamentos/agendamentos.php
@@ -0,0 +1,101 @@
+<?php
+
+global $_GESTOR;
+
+$_GESTOR['modulo-id']							=	'agendamentos';
+$_GESTOR['modulo#'.$_GESTOR['modulo-id']]		=	Array(
+	'versao' => '1.0.0',
+	'plugin' => 'agendamentos',
+	'bibliotecas' => Array('interface','html'),
+	'tabela' => Array(
+		'nome' => 'hosts_agendamentos',
+		'id' => 'id',
+		'id_numerico' => 'id_'.'hosts_agendamentos',
+	),
+);
+
+// ===== Funções Auxiliares
+
+// ===== Funções Principais
+
+function agendamentos_config(){
+	global $_GESTOR;
+	
+	$modulo = $_GESTOR['modulo#'.$_GESTOR['modulo-id']];
+	
+	// ===== Gravar Atualizações no Banco
+	
+	if(isset($_GESTOR['atualizar-banco'])){
+		// ===== Reler URL.
+		
+		gestor_redirecionar_raiz();
+	}
+	
+	// ===== Inclusão Módulo JS
+	
+	gestor_pagina_javascript_incluir();
+	
+	// ===== Interface config finalizar opções
+	
+	$_GESTOR['interface']['config']['finalizar'] = Array(
+		'formulario' => Array(
+			'campos' => Array(
+				
+			),
+		)
+	);
+}
+
+function agendamentos_interfaces_padroes(){
+	global $_GESTOR;
+	
+	$modulo = $_GESTOR['modulo#'.$_GESTOR['modulo-id']];
+	
+	switch($_GESTOR['opcao']){
+		case 'listar':
+			
+		break;
+	}
+}
+
+// ==== Ajax
+
+function agendamentos_ajax_opcao(){
+	global $_GESTOR;
+	
+	$_GESTOR['ajax-json'] = Array(
+		'status' => 'Ok',
+	);
+}
+
+// ==== Start
+
+function agendamentos_start(){
+	global $_GESTOR;
+	
+	gestor_incluir_bibliotecas();
+	
+	if($_GESTOR['ajax']){
+		interface_ajax_iniciar();
+		
+		switch($_GESTOR['ajax-opcao']){
+			//case 'opcao': agendamentos_ajax_opcao(); break;
+		}
+		
+		interface_ajax_finalizar();
+	} else {
+		agendamentos_interfaces_padroes();
+		
+		interface_iniciar();
+		
+		switch($_GESTOR['opcao']){
+			case 'config': agendamentos_config(); break;
+		}
+		
+		interface_finalizar();
+	}
+}
+
+agendamentos_start();
+
+?>
