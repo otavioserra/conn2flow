b8942267d4140fcc88738863320f97625be10aa2
Otávio Campos de Abreu Serra <otavio@b2make.com>
2022-07-28 16:27:59 -0300

    [agendamentos-host] ID do módulo corrigido.

--- remoto/modulos/agendamentos-host/agendamentos-host.php
+++ remoto/modulos/agendamentos-host/agendamentos-host.php
@@ -2,7 +2,7 @@
 
 global $_GESTOR;
 
-$_GESTOR['modulo-id']							=	'agendamentos';
+$_GESTOR['modulo-id']							=	'agendamentos-host';
 $_GESTOR['modulo#'.$_GESTOR['modulo-id']]		=	Array(
 	'versao' => '1.0.88',
 	'numRegistrosPorPagina' => 20,
