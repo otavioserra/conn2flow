586272db6b502bb7f5c57d14f8975d1c698b0c4c
Otávio Campos de Abreu Serra <otavio@b2make.com>
2022-07-29 14:35:45 -0300

    Atualização da versão para v1.4.0 no config.php

--- config.php
+++ config.php
@@ -6,7 +6,7 @@
 
 // ===== Definições de variáveis gerais do gestor.
 
-$_GESTOR['versao']								=	'1.3.5'; // Versão do gestor como um todo.
+$_GESTOR['versao']								=	'1.4.0'; // Versão do gestor como um todo.
 $_GESTOR['id']									=	'entrey-'; // Identificador básico do gestor
 $_GESTOR['linguagem-codigo']					=	'pt-br'; // Linguagem padrão do gestor
 $_GESTOR['host-configuracao-id-modulo']			=	'host-configuracao'; // Identificador módulo de configuração do host.
@@ -163,8 +163,8 @@ $_GESTOR['plataforma'] = Array(
 // ===== Definições do gestor cliente.
 
 $_GESTOR['gestor-cliente'] = Array(
-	'versao' => '1.3.0',
-	'versao_num' => 17,
+	'versao' => '1.4.0',
+	'versao_num' => 18,
 );
 
 // ===== Definição e inclusão de todas as bibliotecas necessárias para o funcionamento do gestor
