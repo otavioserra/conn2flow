9b5fcb281302c015112a4b3f934f944ac6b14f07
Otávio Campos de Abreu Serra <otavio@b2make.com>
2022-08-08 14:48:29 -0300

    [testes] testes em geral do calendário

--- modulos/testes/testes.js
+++ modulos/testes/testes.js
@@ -1,7 +1,40 @@
 $(document).ready(function(){
 	
-	$('.ui.dropdown')
-	  .dropdown()
+	$('.datepicker').datepicker({
+		format: "dd/mm/yy",
+		language: "pt-BR",
+		datesDisabled: gestor.escalas.datasDesabilitadas,
+		multidate: true,
+		startDate: gestor.escalas.startDate,
+		endDate: gestor.escalas.endDate,
+		beforeShowDay: function(date) {
+			var datasDestacadas = gestor.escalas.datasDestacadas;
+			var day = (date.getDate() < 10 ? '0' : '') + date.getDate();
+			var month = ((date.getMonth() + 1) < 10 ? '0' : '') + (date.getMonth() + 1);
+			var year = date.getFullYear();
+			var dateFormated = day + '/' + month + '/' + year;
+			
+			for(var key in datasDestacadas){
+				var dataDestacada = datasDestacadas[key];
+				
+				if(dateFormated == dataDestacada){
+					return {classes: 'highlighted', tooltip: gestor.escalas.highlightedTooltip};
+				}
+			}
+		}
+	})
+	.on('changeDate', function(e) {
+		var dates = '';
+		
+		for(var key in e.dates){
+			var date = e.dates[key];
+			var i = parseInt(key);
+			
+			dates += (dates.length > 0 ? ',':'') + e.format(i);
+		}
+		
+		$('input[name="datas"]').val(dates);
+    })
 	;
 	
 	
--- modulos/testes/testes.php
+++ modulos/testes/testes.php
@@ -4,7 +4,7 @@ global $_GESTOR;
 
 $_GESTOR['modulo-id']							=	'testes';
 $_GESTOR['modulo#'.$_GESTOR['modulo-id']]		=	Array(
-	'versao' => '1.0.3',
+	'versao' => '1.0.8',
 	'bibliotecas' => Array('interface','html','pagina'),
 	'tabela' => Array(
 		'nome' => 'template',
@@ -20,11 +20,23 @@ $_GESTOR['modulo#'.$_GESTOR['modulo-id']]		=	Array(
 function testes_testes(){
 	global $_GESTOR;
 	
-	$pagina = '';
+	$datasStr = '01/08/2022,04/08/2022,08/08/2022';
+	
+	$pagina = '<div class="datepicker" data-date="'.$datasStr.'"></div><input type="hidden" name="datas" value="'.$datasStr.'">';
 	
 	// ===== Área de testes.
 	
+	gestor_pagina_javascript_incluir('<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datepicker/1.9.0/css/bootstrap-datepicker3.standalone.min.css" integrity="sha512-p4vIrJ1mDmOVghNMM4YsWxm0ELMJ/T0IkdEvrkNHIcgFsSzDi/fV7YxzTzb3mnMvFPawuIyIrHcpxClauEfpQg==" crossorigin="anonymous" referrerpolicy="no-referrer" />');
+	gestor_pagina_javascript_incluir('<script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datepicker/1.9.0/js/bootstrap-datepicker.min.js" integrity="sha512-T/tUfKSV1bihCnd+MxKD0Hm1uBBroVYBOYSk1knyvQ9VyZJpc/ALb4P0r6ubwVPSGB2GvjeoMAJJImBG12TiaQ==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>');
+	gestor_pagina_javascript_incluir('<script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datepicker/1.9.0/locales/bootstrap-datepicker.pt-BR.min.js"></script>');
 	
+	$_GESTOR['javascript-vars']['escalas'] = Array(
+		'highlightedTooltip' => "Dia com evento",
+		'startDate' => "01/07/2022",
+		'endDate' => "31/08/2022",
+		'datasDesabilitadas' => Array("02/08/2022","18/08/2022","19/08/2022","20/08/2022","21/08/2022"),
+		'datasDestacadas' => Array("01/08/2022","08/08/2022","10/08/2022","07/08/2022","30/08/2022","31/08/2022"),
+	);
 	
 	// ===== Inclusão Módulo JS
 	
