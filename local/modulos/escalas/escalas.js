d18b6822270779f3beadf7223944975161467f9d
Otávio - Casa <otavio@b2make.com>
2022-09-23 15:07:15 -0300

    [escalas] módulo administrativo primeira implementação.

---
+++ local/modulos/escalas/escalas.js
@@ -0,0 +1,216 @@
+$(document).ready(function(){
+	
+	
+	function escalas_atualizar(p={}){
+		// ===== Caso não exista, criar o objeto de controle.
+		
+		if(!('escalas' in gestor)){
+			gestor.escalas = {};
+		}
+		
+		// ===== Modificar conforme enviado.
+		
+		if('data' in p){gestor.escalas.data = p.data;}
+		if('status' in p){gestor.escalas.status = p.status;}
+		
+		// ===== Esconder manualmente conteiners não necessários devido os componentes do fomantic-ui se auto-mostrarem no início da DOM.
+		
+		if(
+			!('data' in gestor.escalas) || 
+			!('status' in gestor.escalas)
+		){
+			$('.imprimirBtn').hide();
+			$('.tabelaPessoas').hide();
+		}
+		
+		// ===== Mostrar o conteiner de resultados.
+		
+		if(
+			('data' in gestor.escalas)
+		){
+			$('.resultados').show();
+		}
+		
+		// ===== Somente atualizar caso esteja definido 'data' e 'status'.
+		
+		if(
+			('data' in gestor.escalas) && 
+			('status' in gestor.escalas)
+		){
+			// ===== Requisição para atualizar os escalas conforme opção.
+			
+			var opcao = 'escalas';
+			var ajaxOpcao = 'atualizar';
+			
+			$.ajax({
+				type: 'POST',
+				url: gestor.raiz + gestor.moduloId + '/',
+				data: {
+					opcao : opcao,
+					ajax : 'sim',
+					ajaxOpcao : ajaxOpcao,
+					ajaxPagina : 'sim',
+					data : gestor.escalas.data,
+					status : gestor.escalas.status
+				},
+				dataType: 'json',
+				beforeSend: function(){
+					carregando('abrir');
+				},
+				success: function(dados){
+					switch(dados.status){
+						case 'OK':
+							// ===== Montar a tabela.
+							
+							$('.tabelaPessoas').html(dados.tabela);
+							
+							// ===== Atualizar o total de pessoas.
+							
+							$('.totalValor').html(dados.total);
+							
+							// ===== Mostrar ou não o botão imprimir.
+							
+							if(dados.imprimir){
+								$('.imprimirBtn').show();
+							} else {
+								$('.imprimirBtn').hide();
+							}
+							
+							// ===== Mostrar os conteiners de informação dos resultados.
+							
+							$('.totalPessoas').show();
+							$('.tabelaPessoas').show();
+						break;
+						default:
+							console.log('ERROR - '+opcao+' - '+dados.status);
+						
+					}
+					
+					carregando('fechar');
+				},
+				error: function(txt){
+					switch(txt.status){
+						case 401: window.open(gestor.raiz + (txt.responseJSON.redirect ? txt.responseJSON.redirect : "signin/"),"_self"); break;
+						default:
+							console.log('ERROR AJAX - '+opcao+' - Dados:');
+							console.log(txt);
+							carregando('fechar');
+					}
+				}
+			});
+		}
+	}
+	
+	function escalas(){
+		// ===== Configurações do calendário.
+		
+		var calendario = gestor.calendario;
+		
+		// ===== Datas disponíveis para agendamento.
+		
+		var datasDisponiveis = [];
+		
+		for(var data in calendario.datas_disponiveis){
+			var dateObj = new Date(data.replace(/-/g, '\/')); // Bug no objeto Date() do javascript. Basta trocar o '-' por '/' que a data funciona corretamente. Senão fica um dia a mais do dia correto.
+			
+			datasDisponiveis.push(dateObj);
+		}
+		
+		// ===== Calendário ptBR.
+		
+		var calendarPtBR = {
+			days: ['D', 'S', 'T', 'Q', 'Q', 'S', 'S'],
+			months: ['Janeiro', 'Fevereiro', 'Março', 'Abril', 'Maio', 'Junho', 'Júlio', 'Agosto', 'Setembro', 'Outubro', 'Novembro', 'Dezembro'],
+			monthsShort: ['Jan', 'Fev', 'Mar', 'Abr', 'Mai', 'Jun', 'Jul', 'Ago', 'Set', 'Out', 'Nov', 'Dez'],
+			today: 'Hoje',
+			now: 'Agora',
+			am: 'AM',
+			pm: 'PM'
+		};
+		
+		// ===== Variáveis do componente 'calendar'.
+		
+		var calendarDatasOpt = {
+			text: calendarPtBR,
+			type: 'date',
+			inline: true,
+			initialDate: new Date(),
+			minDate: new Date(calendario.ano_inicio+'/01/01'),
+			maxDate: new Date(calendario.ano_fim+'/12/31'),
+			eventClass: 'inverted blue',
+			enabledDates: datasDisponiveis,
+			eventDates: datasDisponiveis,
+			formatter: {
+				date: function (date, settings) {
+					if (!date) return '';
+					
+					var day = (date.getDate() < 10 ? '0' : '') + date.getDate();
+					var month = ((date.getMonth() + 1) < 10 ? '0' : '') + (date.getMonth() + 1);
+					var year = date.getFullYear();
+					
+					return day + '/' + month + '/' + year;
+				}
+			},
+			onChange: function(date,dateFormated,mode){
+				$('.agendamentoData').val(dateFormated);
+				$('.dataSelecionada').find('.dataSelecionadaValor').html(dateFormated);
+				
+				var day = (date.getDate() < 10 ? '0' : '') + date.getDate();
+				var month = ((date.getMonth() + 1) < 10 ? '0' : '') + (date.getMonth() + 1);
+				var year = date.getFullYear();
+				
+				var data = year + '-' + month + '-' + day;
+				
+				escalas_atualizar({data});
+			}
+		}
+		
+		// ===== Iniciar calendário.
+		
+		$('.ui.calendar').calendar(calendarDatasOpt);
+		
+		// ===== Acompanhantes dropdown.
+		
+		$('.ui.dropdown').dropdown({
+			onChange: function(value){
+				escalas_atualizar({status:value});
+			}
+		});
+		
+		// ===== Imprimir.
+		
+		$('.imprimirBtn').on('mouseup tap',function(e){
+			if(e.which != 1 && e.which != 0 && e.which != undefined) return false;
+			
+			window.open(gestor.raiz+"pagina-de-impressao/","Imprimir","menubar=0,location=0,height=700,width=1024");
+		});
+	}
+	
+	function start(){
+		if($('#formEscalas').length > 0){
+			escalas();
+		}
+	}
+	
+	start();
+	
+	function carregando(opcao){
+		switch(opcao){
+			case 'abrir':
+				if(!('carregando' in gestor)){
+					$('.paginaCarregando').dimmer({
+						closable: false
+					});
+					
+					gestor.carregando = true;
+				}
+				
+				$('.paginaCarregando').dimmer('show');
+			break;
+			case 'fechar':
+				$('.paginaCarregando').dimmer('hide');
+			break;
+		}
+	}
+	
+});
---
+++ local/modulos/escalas/escalas.php
@@ -0,0 +1,265 @@
+<?php
+
+global $_GESTOR;
+
+$_GESTOR['modulo-id']							=	'escalas';
+$_GESTOR['modulo#'.$_GESTOR['modulo-id']]		=	Array(
+	'versao' => '1.0.0',
+	'plugin' => 'escalas',
+	'bibliotecas' => Array('interface','html'),
+	'tabela' => Array(
+		'nome' => 'hosts_escalas',
+		'id' => 'id',
+		'id_numerico' => 'id_'.'hosts_escalas',
+		'status' => 'status',
+		'versao' => 'versao',
+		'data_criacao' => 'data_criacao',
+		'data_modificacao' => 'data_modificacao',
+	),
+);
+
+// ===== Funções Auxiliares
+
+function escalas_calendario(){
+	global $_GESTOR;
+	
+	gestor_incluir_biblioteca('formato');
+	gestor_incluir_biblioteca('configuracao');
+	
+	$ano_inicio = date('Y');
+	$hoje = date('Y-m-d');
+	
+	$config = configuracao_hosts_variaveis(Array('modulo' => 'configuracoes-agendamentos'));
+	
+	$dias_semana = (existe($config['dias-semana']) ? explode(',',$config['dias-semana']) : Array());
+	$anos = (existe($config['calendario-anos']) ? (int)$config['calendario-anos'] : 2);
+	if(existe($config['datas-indisponiveis'])) $datas_indisponiveis = (existe($config['datas-indisponiveis-valores']) ? explode('|',$config['datas-indisponiveis-valores']) : Array()); else $datas_indisponiveis = Array();
+	$calendario_ferias_de = (existe($config['calendario-ferias-de']) ? trim($config['calendario-ferias-de']) : '15 December');
+	$calendario_ferias_ate = (existe($config['calendario-ferias-ate']) ? trim($config['calendario-ferias-ate']) : '20 January');
+	
+	$ano_fim = (int)$ano_inicio + $anos;
+	
+	for($i=-1;$i<$anos+1;$i++){
+		$periodo_ferias[] = Array(
+			'inicio' => strtotime($calendario_ferias_de." ".($ano_inicio+$i)),
+			'fim' => strtotime($calendario_ferias_ate." ".($ano_inicio+$i+1)),
+		);
+	}
+	
+	$primeiro_dia = strtotime(date("Y-m-d", time()) . " - 60 day");
+	$ultimo_dia = strtotime(date("Y-m-d", time()) . " + ".$anos." year");
+	
+	$dia = $primeiro_dia;
+	do {
+		$dataFormatada = date('d/m/Y', $dia);
+		$flag = false;
+		
+		if($periodo_ferias){
+			foreach($periodo_ferias as $periodo){
+				if(
+					$dia > $periodo['inicio'] &&
+					$dia < $periodo['fim']
+				){
+					$flag = true;
+					break;
+				}
+			}
+		}
+		
+		if($datas_indisponiveis){
+			foreach($datas_indisponiveis as $di){
+				if($dataFormatada == $di){
+					$flag = true;
+					break;
+				}
+			}
+		}
+		
+		if(!$flag){
+			$flag2 = false;
+			$count_dias = 0;
+			if($dias_semana)
+			foreach($dias_semana as $dia_semana){
+				if($dia_semana == strtolower(date('D',$dia))){
+					$flag2 = true;
+					break;
+				}
+				$count_dias++;
+			}
+			
+			if($flag2){
+				$data = date('Y-m-d', $dia);
+				$datas[$data] = 1;
+			}
+		}
+		
+		$dia += 86400;
+	} while ($dia < $ultimo_dia);
+	
+	$JScalendario['datas_disponiveis'] = $datas;
+	$JScalendario['ano_inicio'] = $ano_inicio;
+	$JScalendario['ano_fim'] = $ano_fim;
+	
+	// ===== Variáveis JS.
+	
+	$_GESTOR['javascript-vars']['calendario'] = $JScalendario;
+}
+
+// ===== Funções Principais
+
+function escalas_administrar(){
+	global $_GESTOR;
+	
+	$modulo = $_GESTOR['modulo#'.$_GESTOR['modulo-id']];
+	
+	// ===== Gravar Atualizações no Banco
+	
+	if(isset($_GESTOR['atualizar-banco'])){
+		// ===== Reler URL.
+		
+		gestor_redirecionar_raiz();
+	}
+	
+	// ===== Adicionar o calendário.
+	
+	escalas_calendario();
+	
+	// ===== Inclusão Módulo JS
+	
+	gestor_pagina_javascript_incluir();
+	
+	// ===== Interface config finalizar opções
+	
+	$_GESTOR['interface']['simples']['finalizar'] = Array(
+		
+	);
+}
+
+function escalas_interfaces_padroes(){
+	global $_GESTOR;
+	
+	$modulo = $_GESTOR['modulo#'.$_GESTOR['modulo-id']];
+	
+	switch($_GESTOR['opcao']){
+		case 'administrar':
+			$_GESTOR['interface-opcao'] = 'simples';
+		break;
+		case 'listar':
+			$_GESTOR['interface'][$_GESTOR['opcao']]['finalizar'] = Array(
+				'banco' => Array(
+					'nome' => $modulo['tabela']['nome'],
+					'campos' => Array(
+						'nome',
+						$modulo['tabela']['data_criacao'],
+						$modulo['tabela']['data_modificacao'],
+					),
+					'id' => $modulo['tabela']['id'],
+					'status' => $modulo['tabela']['status'],
+				),
+				'tabela' => Array(
+					'rodape' => true,
+					'colunas' => Array(
+						Array(
+							'id' => 'nome',
+							'nome' => gestor_variaveis(Array('modulo' => 'interface','id' => 'field-name')),
+							'ordenar' => 'asc',
+						),
+						Array(
+							'id' => $modulo['tabela']['data_criacao'],
+							'nome' => gestor_variaveis(Array('modulo' => 'interface','id' => 'field-date-start')),
+							'formatar' => 'dataHora',
+							'nao_procurar' => true,
+						),
+						Array(
+							'id' => $modulo['tabela']['data_modificacao'],
+							'nome' => gestor_variaveis(Array('modulo' => 'interface','id' => 'field-date-modification')),
+							'formatar' => 'dataHora',
+							'nao_procurar' => true,
+						),
+					),
+				),
+				'opcoes' => Array(
+					'editar' => Array(
+						'url' => 'editar/',
+						'tooltip' => gestor_variaveis(Array('modulo' => 'interface','id' => 'tooltip-button-edit')),
+						'icon' => 'edit',
+						'cor' => 'basic blue',
+					),
+					'ativar' => Array(
+						'opcao' => 'status',
+						'status_atual' => 'I',
+						'status_mudar' => 'A',
+						'tooltip' => gestor_variaveis(Array('modulo' => 'interface','id' => 'tooltip-button-active')),
+						'icon' => 'eye slash',
+						'cor' => 'basic brown',
+					),
+					'desativar' => Array(
+						'opcao' => 'status',
+						'status_atual' => 'A',
+						'status_mudar' => 'I',
+						'tooltip' => gestor_variaveis(Array('modulo' => 'interface','id' => 'tooltip-button-desactive')),
+						'icon' => 'eye',
+						'cor' => 'basic green',
+					),
+					'excluir' => Array(
+						'opcao' => 'excluir',
+						'tooltip' => gestor_variaveis(Array('modulo' => 'interface','id' => 'tooltip-button-delete')),
+						'icon' => 'trash alternate',
+						'cor' => 'basic red',
+					),
+				),
+				'botoes' => Array(
+					'adicionar' => Array(
+						'url' => 'adicionar/',
+						'rotulo' => gestor_variaveis(Array('modulo' => 'interface','id' => 'label-button-insert')),
+						'tooltip' => gestor_variaveis(Array('modulo' => 'interface','id' => 'tooltip-button-insert')),
+						'icon' => 'plus circle',
+						'cor' => 'blue',
+					),
+				),
+			);
+		break;
+	}
+}
+
+// ==== Ajax
+
+function escalas_ajax_opcao(){
+	global $_GESTOR;
+	
+	$_GESTOR['ajax-json'] = Array(
+		'status' => 'Ok',
+	);
+}
+
+// ==== Start
+
+function escalas_start(){
+	global $_GESTOR;
+	
+	gestor_incluir_bibliotecas();
+	
+	if($_GESTOR['ajax']){
+		interface_ajax_iniciar();
+		
+		switch($_GESTOR['ajax-opcao']){
+			//case 'opcao': escalas_ajax_opcao(); break;
+		}
+		
+		interface_ajax_finalizar();
+	} else {
+		escalas_interfaces_padroes();
+		
+		interface_iniciar();
+		
+		switch($_GESTOR['opcao']){
+			case 'administrar': escalas_administrar(); break;
+		}
+		
+		interface_finalizar();
+	}
+}
+
+escalas_start();
+
+?>
