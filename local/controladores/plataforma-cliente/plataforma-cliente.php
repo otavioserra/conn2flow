d7345b794c40e99273b914411f03d114eecffc34
Otávio - Casa <otavio@b2make.com>
2022-10-20 12:12:40 -0300

    [plataforma-cliente] corrigido: inclusão da biblioteca formato.

--- local/controladores/plataforma-cliente/plataforma-cliente.php
+++ local/controladores/plataforma-cliente/plataforma-cliente.php
@@ -287,6 +287,8 @@ function plataforma_cliente_plugin_data_agendamento_confirmar($params = false){
 	$email = $hosts_usuarios['email'];
 	$nome = $hosts_usuarios['nome'];
 	
+	gestor_incluir_biblioteca('formato');
+	
 	$codigo = date('dmY').formato_zero_a_esquerda($id_hosts_agendamentos,6);
 	
 	// ===== Formatar mensagem do email.
