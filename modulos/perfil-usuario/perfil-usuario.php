921fe31b3c43628f0640db62f26da82b8c8980bf
Otávio - Casa <otavio@b2make.com>
2022-12-13 16:32:17 -0300

    correção de erros.

--- modulos/perfil-usuario/perfil-usuario.php
+++ modulos/perfil-usuario/perfil-usuario.php
@@ -4,7 +4,7 @@ global $_GESTOR;
 
 $_GESTOR['modulo-id']							=	'perfil-usuario';
 $_GESTOR['modulo#'.$_GESTOR['modulo-id']]		=	Array(
-	'versao' => '1.1.5',
+	'versao' => '1.1.6',
 	'bibliotecas' => Array('interface','html','usuario'),
 	'tabela' => Array(
 		'nome' => 'usuarios',
