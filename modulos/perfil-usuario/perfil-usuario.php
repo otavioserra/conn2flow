0008a97bb387f837bb511fbeba4847be714a60aa
Otávio - Casa <otavio@b2make.com>
2023-01-02 17:29:35 -0300

    [perfil-usuario] testes: #5

--- modulos/perfil-usuario/perfil-usuario.php
+++ modulos/perfil-usuario/perfil-usuario.php
@@ -749,6 +749,7 @@ function perfil_usuario_signin(){
 				
 				// ===== Verificar se o retorno do servidor é válido, senão não validar o reCAPTCHA
 				
+				echo '_REQUEST>> '.print_r($_REQUEST,true)."<br>";
 				echo 'arrResponse>> '.print_r($arrResponse,true)."<br>";exit;
 				
 				if($arrResponse["success"] == '1' && $arrResponse["action"] == $action && $arrResponse["score"] >= 0.5) {
