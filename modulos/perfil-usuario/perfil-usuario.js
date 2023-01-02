e625fae66ec9d54f8d12ec5386fb98a53658257c
Otávio - Casa <otavio@b2make.com>
2023-01-02 18:53:30 -0300

    [perfil-usuario] testes: #20

--- modulos/perfil-usuario/perfil-usuario.js
+++ modulos/perfil-usuario/perfil-usuario.js
@@ -6,35 +6,40 @@ $(document).ready(function(){
 			
 		var formSelector = '#_gestor-form-signup';
 		var googleRecaptchaDone = false;
+		var submitBtnClicked = false;
 		
 		$.formReiniciar({
 			formOnSuccessCalback : 'reCaptcha',
 			formOnSuccessCalbackFunc : function(){
-				if(typeof gestor.googleRecaptchaActive !== typeof undefined && gestor.googleRecaptchaActive !== false){
+				if('googleRecaptchaActive' in gestor){
 					var action = 'signup'; // Action 
 					var googleSiteKey = gestor.googleRecaptchaSite; // Google Site Key
 					
-					if(!googleRecaptchaDone){
-						grecaptcha.ready(function() {
-							grecaptcha.execute(googleSiteKey, {action: action}).then(function(token) {
-								$(formSelector).append('<input type="hidden" name="token" value="'+token+'">');
-								$(formSelector).append('<input type="hidden" name="action" value="'+action+'">');
-								
-								$.formSubmit({
-									id : 'formOnSuccessCalback',
+					if(submitBtnClicked){
+						if(!googleRecaptchaDone){
+							grecaptcha.ready(function() {
+								grecaptcha.execute(googleSiteKey, {action: action}).then(function(token) {
+									$(formSelector).append('<input type="hidden" name="token" value="'+token+'">');
+									$(formSelector).append('<input type="hidden" name="action" value="'+action+'">');
+									
+									$.formSubmit({
+										id : 'formOnSuccessCalback',
+									});
+									
+									googleRecaptchaDone = true;
 								});
-								
-								googleRecaptchaDone = true;
 							});
-						});
-					} else {
-						$.formSubmit({
-							id : 'formOnSuccessCalback',
-						});
+						} else {
+							$.formSubmit({
+								id : 'formOnSuccessCalback',
+							});
+						}
 					}
 				}
 				
-				return false;
+				if(!submitBtnClicked){
+					return false;
+				}
 			}
 		});
 		
@@ -45,9 +50,9 @@ $(document).ready(function(){
 				return false;
 			}
 			
-			$.formSubmit({
-				id : 'formOnSuccessCalback',
-			});
+			submitBtnClicked = true;
+			
+			$('.ui.form.interfaceFormPadrao').form('submit');
 		});
 		
 	}
@@ -67,16 +72,16 @@ $(document).ready(function(){
 						var action = 'logar'; // Action 
 						var googleSiteKey = gestor.googleRecaptchaSite; // Google Site Key
 						
-						grecaptcha.ready(function() {
-							if(submitBtnClicked){
+						if(submitBtnClicked){
+							grecaptcha.ready(function() {
 								grecaptcha.execute(googleSiteKey, {action: action}).then(function(token) {
 									$(formSelector2).append('<input type="hidden" name="token" value="'+token+'">');
 									$(formSelector2).append('<input type="hidden" name="action" value="'+action+'">');
 									
 									$(formSelector2).unbind('submit').submit();
 								});
-							}
-						});
+							});
+						}
 					}
 					
 					if(!submitBtnClicked){
@@ -85,7 +90,7 @@ $(document).ready(function(){
 				}
 			});
 			
-		$(formSelector2).find('._gestor-form-login-btn').on('mouseup tap',function(e){
+		$(formSelector2).find('button').on('mouseup tap',function(e){
 			if(e.which != 1 && e.which != 0 && e.which != undefined) return false;
 			
 			if($(this).hasClass('disabled')){
@@ -102,9 +107,7 @@ $(document).ready(function(){
 		var formSelector3 = '#_gestor-form-forgot-password';
 		
 		var googleRecaptcha = false;
-		if('googleRecaptchaActive' in gestor){
-			googleRecaptcha = true;
-		}
+		var submitBtnClicked = false;
 		
 		$(formSelector3)
 			.form({
@@ -115,16 +118,20 @@ $(document).ready(function(){
 						var googleSiteKey = gestor.googleRecaptchaSite; // Google Site Key
 						
 						grecaptcha.ready(function() {
-							grecaptcha.execute(googleSiteKey, {action: action}).then(function(token) {
-								$(formSelector3).append('<input type="hidden" name="token" value="'+token+'">');
-								$(formSelector3).append('<input type="hidden" name="action" value="'+action+'">');
-								
-								$(formSelector3).unbind('submit').submit();
-							});
+							if(submitBtnClicked){
+								grecaptcha.execute(googleSiteKey, {action: action}).then(function(token) {
+									$(formSelector3).append('<input type="hidden" name="token" value="'+token+'">');
+									$(formSelector3).append('<input type="hidden" name="action" value="'+action+'">');
+									
+									$(formSelector3).unbind('submit').submit();
+								});
+							}
 						});
 					}
 					
-					return false;
+					if(!submitBtnClicked){
+						return false;
+					}
 				}
 			});
 			
@@ -135,30 +142,14 @@ $(document).ready(function(){
 				return false;
 			}
 			
-			if(!googleRecaptcha){
-				$(formSelector3).unbind('submit').submit();
-			}
+			submitBtnClicked = true;
+			
+			$(formSelector2).form('submit');
 		});
 	}
 	
 	if($('#_gestor-validar-usuario').length > 0){
-		/* function initFingerprintJS(){
-			// Initialize an agent at application startup.
-			const fpPromise = FingerprintJS.load();
-
-			// Get the visitor identifier when you need it.
-			fpPromise
-				.then(fp => fp.get())
-				.then(result => {
-					// This is the visitor identifier:
-					const visitorId = result.visitorId;
-					$('#_gestor-validar-usuario-fingerprint').val(visitorId);
-					$('#_gestor-form-validar-usuario').submit();
-				})
-		}
 		
-		$('#gestor-listener').trigger('carregar_abrir');
-		initFingerprintJS(); */
 	}
 	
 	if($('#_gestor-restrict-area').length > 0){
