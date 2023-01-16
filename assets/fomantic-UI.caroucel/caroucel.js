b225067a3d5b9dbae54b274732b5b6967617aea5
Otávio - Casa <otavio@b2make.com>
2023-01-16 15:54:54 -0300

    Correção de erros.

--- assets/fomantic-UI.caroucel/caroucel.js
+++ assets/fomantic-UI.caroucel/caroucel.js
@@ -9,14 +9,14 @@
         update : function( content ) {  }// !!!
     };
 
-    $.fn.tooltip = function(methodOrOptions) {
+    $.fn.caroucel = function(methodOrOptions) {
         if ( methods[methodOrOptions] ) {
             return methods[ methodOrOptions ].apply( this, Array.prototype.slice.call( arguments, 1 ));
         } else if ( typeof methodOrOptions === 'object' || ! methodOrOptions ) {
             // Default to "init"
             return methods.init.apply( this, arguments );
         } else {
-            $.error( 'Method ' +  methodOrOptions + ' does not exist on jQuery.tooltip' );
+            $.error( 'Method ' +  methodOrOptions + ' does not exist on jQuery.caroucel' );
         }    
     };
 
