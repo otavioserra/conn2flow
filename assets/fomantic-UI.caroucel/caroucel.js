2cc6d420a0b84566ab88a73c60dbcd1c8aa0ca20
Otávio - Casa <otavio@b2make.com>
2023-01-16 16:10:26 -0300

    Correção de erros: #4

--- assets/fomantic-UI.caroucel/caroucel.js
+++ assets/fomantic-UI.caroucel/caroucel.js
@@ -5,10 +5,20 @@
 			// ===== default options.
 			
 			var defaultOptions = {
-				
+				verticalResize : 'auto', // Auto / Value resize of main holder - Accept: 'auto' or 'int value' to fix vertical height
 			};
 			
 			var settings = $.extend(defaultOptions, options);
+			
+			// ===== .
+			
+			if(settings.verticalResize === 'auto'){
+				$(this).css('height','100%');
+			} else {
+				$(this).css('height',settings.verticalResize+'px');
+			}
+			
+			// ===== Show caroucel conteiner.
 			
 			$(this).show();
         },
