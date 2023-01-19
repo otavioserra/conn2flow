9cf6182d905b2d9cc37b377e79a6c2c57fa53ef9
Otávio - Casa <otavio@b2make.com>
2023-01-19 11:13:12 -0300

    caroucel implementação.

--- assets/fomantic-UI.caroucel/caroucel.js
+++ assets/fomantic-UI.caroucel/caroucel.js
@@ -1,35 +1,47 @@
 (function( $ ){
-
-    var methods = {
+	var obj;
+	var settings;
+    var pubMethods = {
         init : function(options) {
+			// ===== Get main object.
+			
+			obj = this;
+			
 			// ===== default options.
 			
 			var defaultOptions = {
 				verticalResize : 'auto', // Auto / Value resize of main holder - Accept: 'auto' or 'int value' to fix vertical height
+				animation: { // Default values of animation slides
+					time: 300, // Time until animation finish
+				}
 			};
 			
-			var settings = $.extend(defaultOptions, options);
+			settings = $.extend(defaultOptions, options);
+			
+			// ===== Internal settings.
+			
+			settings.animating = false; // State of animating or stoped.
 			
 			// ===== .
 			
 			var contHeight = 300;
 			
 			if(settings.verticalResize === 'auto'){
-				$(this).css('height','100%');
+				$(obj).css('height','100%');
 			} else {
-				$(this).css('height',settings.verticalResize+'px');
+				$(obj).css('height',settings.verticalResize+'px');
 				contHeight = parseInt(settings.verticalResize);
 			}
 			
 			// ===== Control center holder.
 			
 			var controlCenter = $('<div class="control-center"></div>');
-			$(this).append(controlCenter);
+			$(obj).append(controlCenter);
 			
 			// ===== Change all items.
 			
 			var first = true;
-			$(this).find('.items').find('.item').each(function(){
+			$(obj).find('.items').find('.item').each(function(){
 				// ===== Apply img bg for all items.
 				
 				if($(this).attr('data-src') !== undefined) {
@@ -53,8 +65,8 @@
 			var controlLeft = $('<div class="control-left"><i class="chevron left inverted link big icon"></i></div>');
 			var controlRight = $('<div class="control-right"><i class="chevron right inverted link big icon"></i></div>');
 			
-			$(this).append(controlLeft);
-			$(this).append(controlRight);
+			$(obj).append(controlLeft);
+			$(obj).append(controlRight);
 			
 			var controlTop = (contHeight) / 2;
 			
@@ -63,7 +75,7 @@
 			
 			// ===== Show caroucel conteiner.
 			
-			$(this).show();
+			$(obj).show();
 			
 			// ===== Apply margin ajusts.
 			
@@ -71,6 +83,24 @@
 			
 			controlLeft.css('top',controlTop+'px');
 			controlRight.css('top',controlTop+'px');
+			
+			// ===== Listeners of buttons.
+			
+			$('.control-left').on('mouseup tap',function(e){
+				if(e.which != 1 && e.which != 0 && e.which != undefined) return false;
+				
+				changeSlide({
+					direction : 'left'
+				});
+			});
+			
+			$('.control-right').on('mouseup tap',function(e){
+				if(e.which != 1 && e.which != 0 && e.which != undefined) return false;
+				
+				changeSlide({
+					direction : 'right'
+				});
+			});
         },
         show : function( ) {    },// IS
         hide : function( ) {  },// GOOD
@@ -78,15 +108,75 @@
     };
 
     $.fn.caroucel = function(methodOrOptions) {
-        if ( methods[methodOrOptions] ) {
-            return methods[ methodOrOptions ].apply( this, Array.prototype.slice.call( arguments, 1 ));
+        if ( pubMethods[methodOrOptions] ) {
+            return pubMethods[ methodOrOptions ].apply( this, Array.prototype.slice.call( arguments, 1 ));
         } else if ( typeof methodOrOptions === 'object' || ! methodOrOptions ) {
             // Default to "init"
-            return methods.init.apply( this, arguments );
+            return pubMethods.init.apply( this, arguments );
         } else {
             $.error( 'Method ' +  methodOrOptions + ' does not exist on jQuery.caroucel' );
         }    
     };
-
-
+	
+	function changeSlide(opt = {}){
+		// ===== Return false if we have less than 2 slides or if other animation was started after.
+		
+		if($(obj).find('.items').find('.item').length < 2 || settings.animating){
+			return false;
+		}
+		
+		// ===== Change state to animating for prevent overflow.
+		
+		settings.animating = true;
+		
+		// ===== Get parent resolution to use as root coordinates for the change animation.
+		
+		var parentWidth = $(obj).outerWidth(true);
+		var parentHeight = $(obj).outerHeight(true);
+		
+		// ===== Get both actual slide and next slide.
+		
+		var actualSlide, nextSlide;
+		
+		var first = true;
+		$(obj).find('.items').find('.item').each(function(){
+			
+			if(first){
+				actualSlide = this;
+				first = false;
+			} else {
+				nextSlide = this;
+				return false;
+			}
+		});
+		
+		// ===== Change slides options before animation and repositioning the actual slides on DOM to last position.
+		
+		actualSlide.css('position','absolute');
+		actualSlide.css('zIndex','1');
+		actualSlide.css('top','0');
+		actualSlide.remove().append(obj);
+		
+		// ===== Animate and change slides based on direction and when was finish return actualSlide do default behavior.
+		
+		var leftEnd;
+		switch(opt.direction){
+			case 'right':
+				actualSlide.css('left','0');
+				leftEnd = parentHeight;
+			break;
+			default:
+				actualSlide.css('left',parentHeight);
+				leftEnd = '0';
+		}
+		
+		actualSlide.animate({
+			left: leftEnd,
+		}, settings.animation.time, function() {
+			actualSlide.css('position','relative');
+			actualSlide.css('zIndex','1');
+			settings.animating = false;
+		});
+	}
+	
 })( jQuery );
