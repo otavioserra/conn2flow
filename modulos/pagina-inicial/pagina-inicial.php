823017494d31ffe19d2c1648635ce64d13b73f4d
Otávio - Casa <otavio@b2make.com>
2023-01-19 11:39:57 -0300

    caroucel implementação: #14

--- modulos/pagina-inicial/pagina-inicial.php
+++ modulos/pagina-inicial/pagina-inicial.php
@@ -4,7 +4,7 @@ global $_GESTOR;
 
 $_GESTOR['modulo-id']							=	'pagina-inicial';
 $_GESTOR['modulo#'.$_GESTOR['modulo-id']]		=	Array(
-	'versao' => '1.0.33',
+	'versao' => '1.0.34',
 );
 
 // ==== Start
