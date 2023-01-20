9604d3ae81ff6e7d38ed92928ef3165046335533
Otávio - Casa <otavio@b2make.com>
2023-01-20 16:09:58 -0300

    layout página inicial: 7

--- modulos/paginas-secundarias/paginas-secundarias.js
+++ modulos/paginas-secundarias/paginas-secundarias.js
@@ -2,6 +2,6 @@ $(document).ready(function(){
 	$('.special.cards .image').dimmer({
 		// As hover is not working on mobile, you might use click on those devices as fallback
 		on: 'ontouchstart' in document.documentElement ? 'click' : 'hover',
-		dimmerName: 'dimmerAPlataforma'
+		dimmerName: true
 	});
 });
