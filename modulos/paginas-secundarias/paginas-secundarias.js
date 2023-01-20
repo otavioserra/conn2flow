c908d059b15216ce11dcfeee3ea24ff09c950313
Otávio - Casa <otavio@b2make.com>
2023-01-20 16:13:14 -0300

    layout página inicial: 9

--- modulos/paginas-secundarias/paginas-secundarias.js
+++ modulos/paginas-secundarias/paginas-secundarias.js
@@ -2,6 +2,5 @@ $(document).ready(function(){
 	$('.special.cards .image').dimmer({
 		// As hover is not working on mobile, you might use click on those devices as fallback
 		on: 'ontouchstart' in document.documentElement ? 'click' : 'hover',
-		dimmerName: true
 	});
 });
--- modulos/paginas-secundarias/paginas-secundarias.php
+++ modulos/paginas-secundarias/paginas-secundarias.php
@@ -4,7 +4,7 @@ global $_GESTOR;
 
 $_GESTOR['modulo-id']							=	'paginas-secundarias';
 $_GESTOR['modulo#'.$_GESTOR['modulo-id']]		=	Array(
-	'versao' => '1.0.2',
+	'versao' => '1.0.3',
 );
 
 // ==== Start
