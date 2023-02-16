cfeb6b91ecb70b3622fcb40a4a5b73e9d3317d45
Otávio - Casa <otavio@b2make.com>
2023-02-16 15:42:26 -0300

    [plugins-hosts] versão inicial

---
+++ modulos/plugins-hosts/plugins-hosts.php
@@ -0,0 +1,482 @@
+<?php
+
+global $_GESTOR;
+
+$_GESTOR['modulo-id']							=	'plugins-hosts';
+$_GESTOR['modulo#'.$_GESTOR['modulo-id']]		=	Array(
+	'versao' => '1.0.0',
+	//'plugin' => 'pluginID',
+	'bibliotecas' => Array('interface','html'),
+	'tabela' => Array(
+		'nome' => 'plugins_hosts',
+		'id' => 'id',
+		'id_numerico' => 'id_'.'plugins_hosts',
+		'status' => 'status',
+		'versao' => 'versao',
+		'data_criacao' => 'data_criacao',
+		'data_modificacao' => 'data_modificacao',
+	),
+);
+
+function plugins_hosts_adicionar(){
+	global $_GESTOR;
+	
+	$modulo = $_GESTOR['modulo#'.$_GESTOR['modulo-id']];
+	
+	// ===== Gravar registro no Banco
+	
+	if(isset($_GESTOR['adicionar-banco'])){
+		$usuario = gestor_usuario();
+		
+		// ===== Validação de campos obrigatórios
+		
+		interface_validacao_campos_obrigatorios(Array(
+			'campos' => Array(
+				Array(
+					'regra' => 'texto-obrigatorio',
+					'campo' => 'nome',
+					'label' => gestor_variaveis(Array('modulo' => $_GESTOR['modulo-id'],'id' => 'form-name-label')),
+				)
+			)
+		));
+		
+		// ===== Definição do identificador
+		
+		$campos = null;
+		$campo_sem_aspas_simples = false;
+		
+		$id = banco_identificador(Array(
+			'id' => banco_escape_field($_REQUEST["nome"]),
+			'tabela' => Array(
+				'nome' => $modulo['tabela']['nome'],
+				'campo' => $modulo['tabela']['id'],
+				'id_nome' => $modulo['tabela']['id_numerico'],
+			),
+		));
+		
+		// ===== Campos gerais
+		
+		banco_insert_name_campo('id',$id);
+		if(isset($_REQUEST['nome'])){ banco_insert_name_campo('nome',$_REQUEST['nome']); }
+		
+		// ===== Campos comuns
+		
+		banco_insert_name_campo($modulo['tabela']['status'],'A');
+		banco_insert_name_campo($modulo['tabela']['versao'],'1');
+		banco_insert_name_campo($modulo['tabela']['data_criacao'],'NOW()',true);
+		banco_insert_name_campo($modulo['tabela']['data_modificacao'],'NOW()',true);
+		
+		banco_insert_name
+		(
+			banco_insert_name_campos(),
+			$modulo['tabela']['nome']
+		);
+		
+		gestor_redirecionar($_GESTOR['modulo-id'].'/editar/?'.$modulo['tabela']['id'].'='.$id);
+	}
+	
+	// ===== Inclusão Módulo JS
+	
+	gestor_pagina_javascript_incluir();
+	
+	// ===== Interface adicionar finalizar opções
+	
+	$_GESTOR['interface']['adicionar']['finalizar'] = Array(
+		'formulario' => Array(
+			'validacao' => Array(
+				Array(
+					'regra' => 'texto-obrigatorio',
+					'campo' => 'nome',
+					'label' => gestor_variaveis(Array('modulo' => $_GESTOR['modulo-id'],'id' => 'form-name-label')),
+				)
+			)
+		)
+	);
+}
+
+function plugins_hosts_editar(){
+	global $_GESTOR;
+	
+	$modulo = $_GESTOR['modulo#'.$_GESTOR['modulo-id']];
+	
+	// ===== Identificador do 
+	
+	$id = $_GESTOR['modulo-registro-id'];
+	
+	// ===== Definição dos campos do banco de dados para editar.
+	
+	$camposBanco = Array(
+		'nome',
+	);
+	
+	$camposBancoPadrao = Array(
+		$modulo['tabela']['status'],
+		$modulo['tabela']['versao'],
+		$modulo['tabela']['data_criacao'],
+		$modulo['tabela']['data_modificacao'],
+	);
+	
+	$camposBancoEditar = array_merge($camposBanco,$camposBancoPadrao);
+	$camposBancoAntes = $camposBanco;
+	
+	// ===== Gravar Atualizações no Banco
+	
+	if(isset($_GESTOR['atualizar-banco'])){
+		// ===== Recuperar o estado dos dados do banco de dados antes de editar.
+		
+		if(!banco_select_campos_antes_iniciar(
+			banco_campos_virgulas($camposBancoAntes)
+			,
+			$modulo['tabela']['nome'],
+			"WHERE ".$modulo['tabela']['id']."='".$id."'"
+			." AND ".$modulo['tabela']['status']."!='D'"
+		)){
+			interface_alerta(Array(
+				'redirect' => true,
+				'msg' => gestor_variaveis(Array('modulo' => $_GESTOR['modulo-id'],'id' => 'alert-database-field-before-error'))
+			));
+			
+			gestor_redirecionar_raiz();
+		}
+		
+		// ===== Validação de campos obrigatórios
+		
+		interface_validacao_campos_obrigatorios(Array(
+			'campos' => Array(
+				Array(
+					'regra' => 'texto-obrigatorio',
+					'campo' => 'nome',
+					'label' => gestor_variaveis(Array('modulo' => $_GESTOR['modulo-id'],'id' => 'form-name-label')),
+				)
+			)
+		));
+		
+		// ===== Valores padrões da tabela e regras para o campo nome
+		
+		$campo = 'nome'; $request = 'nome'; $alteracoes_name = 'name'; if(banco_select_campos_antes($campo) != (isset($_REQUEST[$request]) ? $_REQUEST[$request] : NULL)){
+			$editar = true;
+			banco_update_campo($campo,$_REQUEST[$request],false,true);
+			if(!isset($_REQUEST['_gestor-nao-alterar-id'])){$alterar_id = true;}
+			$alteracoes[] = Array('campo' => 'form-'.$alteracoes_name.'-label', 'valor_antes' => banco_select_campos_antes($campo),'valor_depois' => banco_escape_field($_REQUEST[$request]));
+		}
+		
+		// ===== Se mudar o nome, mudar o identificador do registro
+		
+		if(isset($alterar_id)){
+			$id_novo = banco_identificador(Array(
+				'id' => banco_escape_field($_REQUEST["nome"]),
+				'tabela' => Array(
+					'nome' => $modulo['tabela']['nome'],
+					'campo' => $modulo['tabela']['id'],
+					'id_nome' => $modulo['tabela']['id_numerico'],
+					'id_valor' => interface_modulo_variavel_valor(Array('variavel' => $modulo['tabela']['id_numerico'])),
+				),
+			));
+			
+			$alteracoes[] = Array('campo' => 'field-id', 'valor_antes' => $id,'valor_depois' => $id_novo);
+			banco_update_campo($modulo['tabela']['id'],$id_novo);
+			$_GESTOR['modulo-registro-id'] = $id_novo;
+		}
+		
+		// ===== Atualização dos demais campos.
+		
+		
+		
+		// ===== Se houve alterações, modificar no banco de dados junto com campos padrões de atualização
+		
+		if(isset($editar)){
+			banco_update_campo($modulo['tabela']['versao'],$modulo['tabela']['versao']." + 1",true);
+			banco_update_campo($modulo['tabela']['data_modificacao'],'NOW()',true);
+			
+			banco_update_executar($modulo['tabela']['nome'],"WHERE ".$modulo['tabela']['id']."='".$id."' AND ".$modulo['tabela']['status']."!='D'");
+			
+			// ===== Incluir no histórico as alterações.
+			
+			interface_historico_incluir(Array(
+				'alteracoes' => $alteracoes,
+			));
+		}
+		
+		// ===== Reler URL.
+		
+		gestor_redirecionar($_GESTOR['modulo-id'].'/editar/?'.$modulo['tabela']['id'].'='.(isset($id_novo) ? $id_novo : $id));
+	}
+	
+	// ===== Inclusão Módulo JS
+	
+	gestor_pagina_javascript_incluir();
+	
+	// ===== Selecionar dados do banco de dados
+	
+	$retorno_bd = banco_select_editar
+	(
+		banco_campos_virgulas($camposBancoEditar)
+		,
+		$modulo['tabela']['nome'],
+		"WHERE ".$modulo['tabela']['id']."='".$id."'"
+		." AND ".$modulo['tabela']['status']."!='D'"
+	);
+	
+	if($_GESTOR['banco-resultado']){
+		$nome = (isset($retorno_bd['nome']) ? $retorno_bd['nome'] : '');
+		
+		$_GESTOR['pagina'] = modelo_var_troca_tudo($_GESTOR['pagina'],'#nome#',$nome);
+		
+		// ===== Popular os metaDados
+		
+		$status_atual = (isset($retorno_bd[$modulo['tabela']['status']]) ? $retorno_bd[$modulo['tabela']['status']] : '');
+		
+		if(isset($retorno_bd[$modulo['tabela']['data_criacao']])){ $metaDados[] = Array('titulo' => gestor_variaveis(Array('modulo' => 'interface','id' => 'field-date-start')),'dado' => interface_formatar_dado(Array('dado' => $retorno_bd[$modulo['tabela']['data_criacao']], 'formato' => 'dataHora'))); }
+		if(isset($retorno_bd[$modulo['tabela']['data_modificacao']])){ $metaDados[] = Array('titulo' => gestor_variaveis(Array('modulo' => 'interface','id' => 'field-date-modification')),'dado' => interface_formatar_dado(Array('dado' => $retorno_bd[$modulo['tabela']['data_modificacao']], 'formato' => 'dataHora'))); }
+		if(isset($retorno_bd[$modulo['tabela']['versao']])){ $metaDados[] = Array('titulo' => gestor_variaveis(Array('modulo' => 'interface','id' => 'field-version')),'dado' => $retorno_bd[$modulo['tabela']['versao']]); }
+		if(isset($retorno_bd[$modulo['tabela']['status']])){ $metaDados[] = Array('titulo' => gestor_variaveis(Array('modulo' => 'interface','id' => 'field-status')),'dado' => ($retorno_bd[$modulo['tabela']['status']] == 'A' ? '<div class="ui center aligned green message"><b>'.gestor_variaveis(Array('modulo' => 'interface','id' => 'field-status-active')).'</b></div>' : '').($retorno_bd[$modulo['tabela']['status']] == 'I' ? '<div class="ui center aligned brown message"><b>'.gestor_variaveis(Array('modulo' => 'interface','id' => 'field-status-inactive')).'</b></div>' : '')); }
+	} else {
+		gestor_redirecionar_raiz();
+	}
+	
+	// ===== Interface editar finalizar opções
+	
+	$_GESTOR['interface']['editar']['finalizar'] = Array(
+		'id' => $id,
+		'metaDados' => $metaDados,
+		'banco' => Array(
+			'nome' => $modulo['tabela']['nome'],
+			'id' => $modulo['tabela']['id'],
+			'status' => $modulo['tabela']['status'],
+		),
+		'botoes' => Array(
+			'adicionar' => Array(
+				'url' => $_GESTOR['url-raiz'].$_GESTOR['modulo-id'].'/adicionar/',
+				'rotulo' => gestor_variaveis(Array('modulo' => 'interface','id' => 'label-button-insert')),
+				'tooltip' => gestor_variaveis(Array('modulo' => 'interface','id' => 'tooltip-button-insert')),
+				'icon' => 'plus circle',
+				'cor' => 'blue',
+			),
+			'status' => Array(
+				'url' => $_GESTOR['url-raiz'].$_GESTOR['modulo-id'].'/?opcao=status&'.$modulo['tabela']['status'].'='.($status_atual == 'A' ? 'I' : 'A' ).'&'.$modulo['tabela']['id'].'='.$id.'&redirect='.urlencode($_GESTOR['modulo-id'].'/editar/?'.$modulo['tabela']['id'].'='.$id),
+				'rotulo' => ($status_atual == 'A' ? gestor_variaveis(Array('modulo' => 'interface','id' => 'label-button-desactive')) : gestor_variaveis(Array('modulo' => 'interface','id' => 'label-button-active')) ),
+				'tooltip' => ($status_atual == 'A' ? gestor_variaveis(Array('modulo' => 'interface','id' => 'tooltip-button-desactive')) : gestor_variaveis(Array('modulo' => 'interface','id' => 'tooltip-button-active'))),
+				'icon' => ($status_atual == 'A' ? 'eye' : 'eye slash' ),
+				'cor' => ($status_atual == 'A' ? 'green' : 'brown' ),
+			),
+			'excluir' => Array(
+				'url' => $_GESTOR['url-raiz'].$_GESTOR['modulo-id'].'/?opcao=excluir&'.$modulo['tabela']['id'].'='.$id,
+				'rotulo' => gestor_variaveis(Array('modulo' => 'interface','id' => 'label-button-delete')),
+				'tooltip' => gestor_variaveis(Array('modulo' => 'interface','id' => 'tooltip-button-delete')),
+				'icon' => 'trash alternate',
+				'cor' => 'red',
+			),
+		),
+		'formulario' => Array(
+			'validacao' => Array(
+				Array(
+					'regra' => 'texto-obrigatorio',
+					'campo' => 'nome',
+					'label' => gestor_variaveis(Array('modulo' => $_GESTOR['modulo-id'],'id' => 'form-name-label')),
+				)
+			)
+		)
+	);
+}
+
+function plugins_hosts_editar_incomum(){
+	global $_GESTOR;
+	
+	$modulo = $_GESTOR['modulo#'.$_GESTOR['modulo-id']];
+	
+	// ===== Identificador do registro
+	
+	$id = $_GESTOR['modulo-registro-id'];
+	
+	// ===== Selecionar dados do banco de dados.
+	
+	$retorno_bd = banco_select_editar
+	(
+		banco_campos_virgulas(Array(
+			'campo',
+		))
+		,
+		$modulo['tabela']['nome'],
+		"WHERE ".$modulo['tabela']['id']."='".$id."'"
+		." AND ".$modulo['tabela']['status']."!='D'"
+	);
+	
+	if($_GESTOR['banco-resultado']){
+		
+	}
+	
+	// ===== Inclusão Módulo JS
+	
+	gestor_pagina_javascript_incluir();
+	
+	// ===== Interface adicionar finalizar opções
+	
+	$_GESTOR['interface'][$_GESTOR['interface-opcao']]['finalizar'] = Array(
+		'formulario' => Array(
+			'validacao' => Array(
+				Array(
+					'regra' => 'texto-obrigatorio',
+					'campo' => 'nome',
+					'label' => gestor_variaveis(Array('modulo' => $_GESTOR['modulo-id'],'id' => 'form-name-label')),
+				)
+			)
+		)
+	);
+}
+
+function plugins_hosts_status(){
+	global $_GESTOR;
+	
+	$id = $_GESTOR['modulo-registro-id'];
+	$status = $_GESTOR['modulo-registro-status'];
+	
+	
+}
+
+function plugins_hosts_excluir(){
+	global $_GESTOR;
+	
+	$id_numerico = $_GESTOR['modulo-registro-id-numerico'];
+	
+	
+}
+
+function plugins_hosts_interfaces_padroes(){
+	global $_GESTOR;
+	
+	$modulo = $_GESTOR['modulo#'.$_GESTOR['modulo-id']];
+	
+	switch($_GESTOR['opcao']){
+		case 'status':
+			$_GESTOR['interface'][$_GESTOR['opcao']]['finalizar'] = Array(
+				'callbackFunction' => 'plugins_hosts_status',
+			);
+		break;
+		case 'excluir':
+			$_GESTOR['interface'][$_GESTOR['opcao']]['finalizar'] = Array(
+				'callbackFunction' => 'plugins_hosts_excluir',
+			);
+		break;
+		case 'editar-incomum':
+			$_GESTOR['interface-opcao'] = 'editar-incomum';
+		break;
+		case 'listar':
+			$_GESTOR['interface'][$_GESTOR['opcao']]['finalizar'] = Array(
+				'banco' => Array(
+					'nome' => $modulo['tabela']['nome'],
+					'campos' => Array(
+						'nome',
+						$modulo['tabela']['data_criacao'],
+						$modulo['tabela']['data_modificacao'],
+					),
+					'id' => $modulo['tabela']['id'],
+					'status' => $modulo['tabela']['status'],
+				),
+				'tabela' => Array(
+					'rodape' => true,
+					'colunas' => Array(
+						Array(
+							'id' => 'nome',
+							'nome' => gestor_variaveis(Array('modulo' => 'interface','id' => 'field-name')),
+							'ordenar' => 'asc',
+						),
+						Array(
+							'id' => $modulo['tabela']['data_criacao'],
+							'nome' => gestor_variaveis(Array('modulo' => 'interface','id' => 'field-date-start')),
+							'formatar' => 'dataHora',
+							'nao_procurar' => true,
+						),
+						Array(
+							'id' => $modulo['tabela']['data_modificacao'],
+							'nome' => gestor_variaveis(Array('modulo' => 'interface','id' => 'field-date-modification')),
+							'formatar' => 'dataHora',
+							'nao_procurar' => true,
+						),
+					),
+				),
+				'opcoes' => Array(
+					'editar' => Array(
+						'url' => 'editar/',
+						'tooltip' => gestor_variaveis(Array('modulo' => 'interface','id' => 'tooltip-button-edit')),
+						'icon' => 'edit',
+						'cor' => 'basic blue',
+					),
+					'ativar' => Array(
+						'opcao' => 'status',
+						'status_atual' => 'I',
+						'status_mudar' => 'A',
+						'tooltip' => gestor_variaveis(Array('modulo' => 'interface','id' => 'tooltip-button-active')),
+						'icon' => 'eye slash',
+						'cor' => 'basic brown',
+					),
+					'desativar' => Array(
+						'opcao' => 'status',
+						'status_atual' => 'A',
+						'status_mudar' => 'I',
+						'tooltip' => gestor_variaveis(Array('modulo' => 'interface','id' => 'tooltip-button-desactive')),
+						'icon' => 'eye',
+						'cor' => 'basic green',
+					),
+					'excluir' => Array(
+						'opcao' => 'excluir',
+						'tooltip' => gestor_variaveis(Array('modulo' => 'interface','id' => 'tooltip-button-delete')),
+						'icon' => 'trash alternate',
+						'cor' => 'basic red',
+					),
+				),
+				'botoes' => Array(
+					'adicionar' => Array(
+						'url' => 'adicionar/',
+						'rotulo' => gestor_variaveis(Array('modulo' => 'interface','id' => 'label-button-insert')),
+						'tooltip' => gestor_variaveis(Array('modulo' => 'interface','id' => 'tooltip-button-insert')),
+						'icon' => 'plus circle',
+						'cor' => 'blue',
+					),
+				),
+			);
+		break;
+	}
+}
+
+// ==== Ajax
+
+function plugins_hosts_ajax_opcao(){
+	global $_GESTOR;
+	
+	$_GESTOR['ajax-json'] = Array(
+		'status' => 'Ok',
+	);
+}
+
+// ==== Start
+
+function plugins_hosts_start(){
+	global $_GESTOR;
+	
+	gestor_incluir_bibliotecas();
+	
+	if($_GESTOR['ajax']){
+		interface_ajax_iniciar();
+		
+		switch($_GESTOR['ajax-opcao']){
+			//case 'opcao': plugins_hosts_ajax_opcao(); break;
+		}
+		
+		interface_ajax_finalizar();
+	} else {
+		plugins_hosts_interfaces_padroes();
+		
+		interface_iniciar();
+		
+		switch($_GESTOR['opcao']){
+			case 'adicionar': plugins_hosts_adicionar(); break;
+			case 'editar': plugins_hosts_editar(); break;
+		}
+		
+		interface_finalizar();
+	}
+}
+
+plugins_hosts_start();
+
+?>
